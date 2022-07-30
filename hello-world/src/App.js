1b5198a69e50fafbbbc7f7bf7d9b3d894937b9c3
SaiVamshiK <saivamshi.k.24@gmail.com>
2022-07-30 18:13:04 +0530

    Modifying JSC

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,26 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
   const str = "Hello world!!!";
+  // We can return only a single <> </> in the return of a component in React.
+  // If we have multiple opening and closing tags then wrap all in 1 div or use <> </>
+  // Using <> </> makes sure that in the browser all the opening and closing tags are separate not wrapped in one div
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          {str}
-          {console.log("We can write JS code in jsx using {} braces and they must be one line JS code only")}
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            {str}
+            {console.log("We can write JS code in jsx using {} braces and they must be one line JS code only")}
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+      <span>
+
+      </span>
+    </>
   );
 }
 
